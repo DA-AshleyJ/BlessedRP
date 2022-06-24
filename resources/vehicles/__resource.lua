b06af6be035fd9513d12aadaf5e6bed2b9f30824
AshleyJ1994 <ajohnson0894@icloud.com>
2022-06-24 12:20:47 +0100

    Committing Vehicle Pack #1

---
+++ resources/vehicles/__resource.lua
@@ -0,0 +1,35 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+files {
+    '/**/vehicles.meta',
+    '/**/carvariations.meta',
+    '/**/carcols.meta',
+    '/**/handling.meta',
+    '/**/vehiclelayouts.meta',
+    '/**/**/vehicles.meta',
+    '/**/**/carvariations.meta',
+    '/**/**/carcols.meta',
+    '/**/**/handling.meta',
+    '/**/**/vehiclelayouts.meta',
+    '/**/**/*.rel',
+    '/**/*.rel',
+    '/**/**/*.awc',
+    '/**/*.awc'
+
+}
+
+data_file 'HANDLING_FILE' '/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '/**/vehicles.meta'
+data_file 'CARCOLS_FILE' '/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' '/**/vehiclelayouts.meta'
+data_file 'HANDLING_FILE' '/**/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '/**/**/vehicles.meta'
+data_file 'CARCOLS_FILE' '/**/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '/**/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' '/**/**/vehiclelayouts.meta'
+data_file 'AUDIO_GAMEDATA' '/**/**/*game.dat'
+data_file 'AUDIO_SOUNDDATA' '/**/**/*sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc*'
+data_file 'AUDIO_GAMEDATA' '/**/*game.dat'
+data_file 'AUDIO_SOUNDDATA' '/**/*sounds.dat'
---
+++ resources/vehicles/stream/bcfl/ReadmeADDON.txt
@@ -0,0 +1,24 @@
+==================Installation==================
+[dlclist.xml part 1]
+1. Extract  "dlclist.xml" with openIV from -----> \update\update.rpf\common\data
+2. Add new line "<Item>dlcpacks:\bcfl\</Item>" to dlclist.xml
+3. Save, and replace the old one at ------> \update\update.rpf\common\data
+
+[dlc data part 2]
+1. Create a folder under \mods\update\x64\dlcpacks\ called "bcfl"
+2. drop dlc.rpf into the folder you just created.
+3. Have Fun!
+
+=========SPAWNING THE NEW ADDED CARS=========
+I recommend you to use Enhanced Native Trainer or Simple Trainer, etc with "spawn car by name" function.
+Select this function and input the car name to spawn it
+Ingame car names
+
+Bagged Customs Featherlight Custom Hauler --------> Type this name: bcfl
+
+
+Toggle boot to lower ramp
+Toggle bonnet to raise lift
+Toggle Right Rear door to open side door
+
+Livery support 
