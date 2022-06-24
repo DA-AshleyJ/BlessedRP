5d546ab11c76ccf76bd11a6c900509a43edb3c91
AshleyJ1994 <ajohnson0894@icloud.com>
2022-06-24 11:20:07 +0100

    committing the hoes

---
+++ resources/trucks/__resource.lua
@@ -0,0 +1,51 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files {
+	"actros_handling.meta",
+	"actros_vehicles.meta",
+	"actros_carvariations.meta",
+
+	"daf_handling.meta",
+	"daf_vehicles.meta",
+	"daf_carvariations.meta",
+
+	"man_handling.meta",
+	"man_vehicles.meta",
+	"man_carvariations.meta",
+
+	"t680_handling.meta",
+	"t680_vehicles.meta",
+	"t680_carvariations.meta",
+
+	"vnl780_handling.meta",
+	"vnl780_vehicles.meta",
+	"vnl780_carvariations.meta",
+
+	"w900_handling.meta",
+	"w900_vehicles.meta",
+	"w900_carvariations.meta",
+}
+
+data_file "HANDLING_FILE" "actros_handling.meta"
+data_file "VEHICLE_METADATA_FILE" "actros_vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "actros_carvariations.meta"
+
+data_file "HANDLING_FILE" "daf_handling.meta"
+data_file "VEHICLE_METADATA_FILE" "daf_vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "daf_carvariations.meta"
+
+data_file "HANDLING_FILE" "man_handling.meta"
+data_file "VEHICLE_METADATA_FILE" "man_vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "man_carvariations.meta"
+
+data_file "HANDLING_FILE" "t680_handling.meta"
+data_file "VEHICLE_METADATA_FILE" "t680_vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "t680_carvariations.meta"
+
+data_file "HANDLING_FILE" "vnl780_handling.meta"
+data_file "VEHICLE_METADATA_FILE" "vnl780_vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "vnl780_carvariations.meta"
+
+data_file "HANDLING_FILE" "w900_handling.meta"
+data_file "VEHICLE_METADATA_FILE" "w900_vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "w900_carvariations.meta"
