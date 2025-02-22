e200503e19f13dbf40a4bf87c11a296d2c0480a4
Ashley Johnson <114417853+DA-AshleyJ@users.noreply.github.com>
2025-02-22 20:18:34 +0000

    Delete resources/[gabz] directory

--- resources/[gabz]/uj_cayoPerico/fxmanifest.lua
+++
@@ -1,7 +0,0 @@
-fx_version "bodacious"
-game "gta5"
-
-this_is_a_map "yes"
-
-data_file ('DLC_ITYP_REQUEST') ('stream/int_cayo_props.ytyp')
-data_file ('DLC_ITYP_REQUEST') ('stream/med/int_cayo_med_props.ytyp')
